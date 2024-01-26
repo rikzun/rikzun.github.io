420b36908251da4f261ac30a39b73ea617115885
Михаил Якименко <rik.zunqq@gmail.com>
2024-01-26 23:11:24 +0300

    Update ReadonlyPre.tsx

--- src/components/ReadonlyPre/ReadonlyPre.tsx
+++ src/components/ReadonlyPre/ReadonlyPre.tsx
@@ -1,10 +1,13 @@
 import { array, trimStart } from 'src/utils'
 import './ReadonlyPre.styles.scss'
-import type { SyntheticEvent, KeyboardEvent } from 'react'
+import { type SyntheticEvent, type KeyboardEvent, useState, useEffect, createRef } from 'react'
 
 interface ReadonlyPreProps { value: string }
 
 export function ReadonlyPre(props: ReadonlyPreProps) {
+    const ref = createRef<HTMLPreElement>()
+    const [state, setState] = useState('')
+
     const acceptableKeyCodes = [
         'Tab',
         'Home',
@@ -20,8 +23,23 @@ export function ReadonlyPre(props: ReadonlyPreProps) {
 
     const preventDefault = (e: SyntheticEvent) => e.preventDefault()
     const onKeyDown = (e: KeyboardEvent) => {
-        if (!acceptableKeyCodes.includes(e.code)) preventDefault(e)
+        if (!acceptableKeyCodes.includes(e.code)) {
+            setState(e.code)
+
+            preventDefault(e)
+            return
+        }
     }
+
+    useEffect(() => {
+        Object.keys(window)
+            .filter((key) => key.startsWith('on'))
+            .filter((event) => !event.includes("pointer") && !event.includes("mouse"))
+            .forEach((event) => {
+                //@ts-ignore
+                ref.current!![event] = (e: any) => console.log(e.type)
+            })
+    }, [])
 
     /* {`{
                 kekw
@@ -30,6 +48,7 @@ export function ReadonlyPre(props: ReadonlyPreProps) {
 
     return (
         <pre
+            ref={ref}
             className="container"
             spellCheck={false}
             onCut={preventDefault}
@@ -40,9 +59,11 @@ export function ReadonlyPre(props: ReadonlyPreProps) {
         >
             {props.value.split('\n').map((rawLine) => {
                 const line = trimStart(rawLine, 4).split(' ')
-                console.log(line)
+                // console.log(line)
                 return <span>{line}</span>
             })}
+            <br></br>
+            {state}
         </pre>
     )
 }
