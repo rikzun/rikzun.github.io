58f7261b0c428803e3191429789e556332e90bd4
Михаил Якименко <rik.zunqq@gmail.com>
2024-01-27 02:11:03 +0300

    final

--- src/components/ReadonlyPre/ReadonlyPre.tsx
+++ src/components/ReadonlyPre/ReadonlyPre.tsx
@@ -1,14 +1,13 @@
 import { array, trimStart } from 'src/utils'
 import './ReadonlyPre.styles.scss'
 import type { SyntheticEvent, KeyboardEvent, FormEvent } from 'react'
-import { useState, createRef, Fragment } from 'react'
+import { useState } from 'react'
 
 interface ReadonlyPreProps { value: string }
 
 export function ReadonlyPre(props: ReadonlyPreProps) {
-    const ref = createRef<HTMLPreElement>()
-    const [line, setLine] = useState<[string, string][]>([])
-    const println = (value: any, color: string = 'wheat') => setLine((v) => [...v, [value, color]])
+    const [key, setKey] = useState("")
+    const updateKey = () => setKey(crypto.randomUUID())
 
     const acceptableKeyCodes = [
         'Tab',
@@ -30,25 +29,12 @@ export function ReadonlyPre(props: ReadonlyPreProps) {
     }
 
     const onKeyDown = (e: KeyboardEvent) => {
-        if (e.keyCode == 229) {
-            println(e.cancelable)
-            const selection = window.getSelection()!
-            const range = selection.getRangeAt(0)
-            const offset = range.startOffset - 1
-
-            if (offset) {
-                range.setStart(range.startContainer, offset)
-                range.collapse(true)
-                selection.removeAllRanges()
-                selection.addRange(range)
-            }
-        }
-
         if (!acceptableKeyCodes.includes(e.code)) return eventCancel(e)
     }
 
     const onInput = (e: FormEvent<HTMLPreElement>) => {
         if (e.cancelable) return eventCancel(e)
+        updateKey()
     }
 
     /* {`{
@@ -58,7 +44,6 @@ export function ReadonlyPre(props: ReadonlyPreProps) {
 
     return (
         <pre
-            ref={ref}
             className="container"
             spellCheck={false}
             onCut={eventCancel}
@@ -66,22 +51,15 @@ export function ReadonlyPre(props: ReadonlyPreProps) {
             onBeforeInput={eventCancel}
             onInput={onInput}
             onKeyDown={onKeyDown}
+            key={key}
             contentEditable
             suppressContentEditableWarning
         >
-            14{props.value.split('\n').map((rawLine) => {
+            {props.value.split('\n').map((rawLine) => {
                 const line = trimStart(rawLine, 4).split(' ')
                 // console.log(line)
                 return <span>{line}</span>
             })}
-            <br />
-
-            {line.map(([value, color], i) => (
-                <Fragment key={value + color + i}>
-                    <span style={{color}}>{value}</span>
-                    <br />
-                </Fragment>
-            ))}
         </pre>
     )
 }
