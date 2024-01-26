45423a18392c33ff7e3ae6693fc6b5033a0126e6
Михаил Якименко <rik.zunqq@gmail.com>
2024-01-26 23:35:48 +0300

    test end

--- src/components/ReadonlyPre/ReadonlyPre.tsx
+++ src/components/ReadonlyPre/ReadonlyPre.tsx
@@ -1,14 +1,10 @@
 import { array, trimStart } from 'src/utils'
 import './ReadonlyPre.styles.scss'
-import { type SyntheticEvent, type KeyboardEvent, useState, useEffect, createRef, Fragment } from 'react'
+import type { SyntheticEvent, KeyboardEvent } from 'react'
 
 interface ReadonlyPreProps { value: string }
 
 export function ReadonlyPre(props: ReadonlyPreProps) {
-    const ref = createRef<HTMLPreElement>()
-    const [keys, setKeys] = useState<string[]>([])
-    const [events, setEvents] = useState<string[]>([])
-
     const acceptableKeyCodes = [
         'Tab',
         'Home',
@@ -24,20 +20,8 @@ export function ReadonlyPre(props: ReadonlyPreProps) {
 
     const preventDefault = (e: SyntheticEvent) => e.preventDefault()
     const onKeyDown = (e: KeyboardEvent) => {
-        setKeys((v) => [...v, e.code])
-
         if (!acceptableKeyCodes.includes(e.code)) return preventDefault(e)
     }
-
-    useEffect(() => {
-        Object.keys(window)
-            .filter((key) => key.startsWith('on'))
-            .filter((event) => !event.includes("pointer") && !event.includes("mouse"))
-            .forEach((event) => {
-                //@ts-ignore
-                ref.current!![event] = (e: any) => setEvents((v) => [...v, e.type])
-            })
-    }, [])
 
     /* {`{
                 kekw
@@ -46,12 +30,12 @@ export function ReadonlyPre(props: ReadonlyPreProps) {
 
     return (
         <pre
-            ref={ref}
             className="container"
             spellCheck={false}
             onCut={preventDefault}
             onPaste={preventDefault}
             onKeyDown={onKeyDown}
+            onInput={preventDefault}
             contentEditable
             suppressContentEditableWarning
         >
@@ -60,22 +44,6 @@ export function ReadonlyPre(props: ReadonlyPreProps) {
                 // console.log(line)
                 return <span>{line}</span>
             })}
-
-            <br></br>
-            {keys.map((v) => (
-                <Fragment key={v}>
-                    <span className='key'>{v}</span>
-                    <br></br>
-                </Fragment>
-            ))}
-            
-            <br></br>
-            {events.map((v) => (
-                <Fragment key={v}>
-                    <span className='event'>{v}</span>
-                    <br></br>
-                </Fragment>
-            ))}
         </pre>
     )
 }
